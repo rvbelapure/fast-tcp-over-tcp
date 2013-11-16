9a412aefb036fd40677f1b696460b39d783f7ad7
anshumang <goswami.anshuman@gmail.com>
2013-11-16 16:20:54 -0500

    connect first pass done

--- tcp-base/libtcp.c
+++ tcp-base/libtcp.c
@@ -1,7 +1,9 @@
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <assert.h>
 #include <stdint.h>
 #include "tcp.h"
+#include "tcputils.h"
 
 int gt_listen(int sockfd, int backlog) {
 
@@ -11,7 +13,31 @@ int gt_bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen)
 {
 	bind(sockfd, addr, addrlen);
 }
-int gt_connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {}
+int gt_connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
+
+	int sockfd_conn = connect(sockfd, addr, addrlen);
+
+	tcp_packet_t *syn_pkt = (tcp_packet_t *)malloc(sizeof(tcp_packet_t));
+	syn_pkt->ubuf = NULL;
+	syn_pkt->ulen = 0;
+	syn_pkt->uflags = 0;
+	syn_pkt->gt_flags = SYN_FLAG;
+	gt_send_size(sockfd_conn, (const void *)syn_pkt);
+//SYN is sent at this point
+
+	tcp_packet_t syn_ack_pkt = NULL;
+	gt_recv_size(sockfd_conn, syn_ack_pkt);
+	assert(syn_ack_pkt->gt_flags & (SYN_FLAG & ACK_FLAG) == 1);
+//SYN, ACK is received at this point
+
+	tcp_packet_t *ack_pkt = (tcp_packet_t *)malloc(sizeof(tcp_packet_t));
+	ack_pkt->ubuf = NULL;
+	ack_pkt->ulen = 0;
+	ack_pkt->uflags = 0;
+	ack_pkt->gt_flags = ACK_FLAG;
+	gt_send_size(sockfd_conn, (const void *)ack_pkt);
+}
+
 int gt_accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen) 
 {
 	int new_sockid = accept(sockfd, addr, addrlen);
