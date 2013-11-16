469393167a46863cf81bfcddcf59e3fe6b09be35
Raghavendra Belapure <raghavendra@raghavendra-laptop>
2013-11-16 14:13:02 -0500

    added header

---
+++ tcp-base/tcp.h
@@ -0,0 +1,15 @@
+#ifndef __GTTCP_H
+#define __GTTCP_H
+
+#include <sys/types.h>          /* See NOTES */
+#include <sys/socket.h>
+
+int gt_listen(int sockfd, int backlog);
+int gt_bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
+int gt_connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
+int gt_accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen);
+ssize_t gt_send(int sockfd, const void *buf, size_t len, int flags);
+size_t gt_recv(int sockfd, void *buf, size_t len, int flags);
+int gt_close(int sockfd);
+
+#endif
