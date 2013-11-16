963b6a544363f040b76760f3051f9bcaba6540e2
Akash Gangil <akash.gangil@outlook.com>
2013-11-16 17:01:27 -0500

    fixing compilation errors

--- tcp-base/tcp.h
+++ tcp-base/tcp.h
@@ -15,7 +15,7 @@ int gt_bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
 int gt_connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
 int gt_accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen);
 ssize_t gt_send(int sockfd, const void *buf, size_t len, int flags);
-size_t gt_recv(int sockfd, void *buf, size_t len, int flags);
+ssize_t gt_recv(int sockfd, void *buf, size_t len, int flags);
 int gt_close(int sockfd);
 
 typedef struct _tcp_packet 
