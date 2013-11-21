966395237ed157b2170ae63b58f04cca7fe7887c
Raghavendra Belapure <raghavendra@raghavendra-laptop>
2013-11-20 20:55:23 -0500

    implemented Fast connect

--- tcp-fast-open/libtcp.c
+++ tcp-fast-open/libtcp.c
@@ -5,12 +5,13 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
 
 #include "tcp.h"
 #include "tcputils.h"
 
-sock_descriptor_t * gt_socket(int domain, int type, int protocol)
-{
+sock_descriptor_t * gt_socket(int domain, int type, int protocol) {
+	/* Allocate new socket descriptor. create a socket and return the descriptor */
 	sock_descriptor_t * sockfd = (sock_descriptor_t *) malloc(sizeof(sock_descriptor_t));
 	sockfd->sockfd = socket(domain, type, protocol);
 	sockfd->data = NULL;
@@ -22,38 +23,36 @@ sock_descriptor_t * gt_socket(int domain, int type, int protocol)
 int gt_listen(sock_descriptor_t *sockfd, int backlog) {
 	return listen(sockfd->sockfd, backlog);
 }
-int gt_bind(sock_descriptor_t *sockfd, const struct sockaddr *addr, socklen_t addrlen) 
-{
+int gt_bind(sock_descriptor_t *sockfd, const struct sockaddr *addr, socklen_t addrlen) {
 	return bind(sockfd->sockfd, addr, addrlen);
 }
-int gt_connect(sock_descriptor_t *sockfd, const struct sockaddr *addr, socklen_t addrlen) {
-
+int gt_connect(sock_descriptor_t *sockfd, const struct sockaddr *addr, socklen_t addrlen, 
+						unsigned long cookie, char * udata, ssize_t ulen) {
+	/* Create connection to socket owned by application thread of server.
+	 * This socket will be returned to client application.*/
 	int sockfd_conn = sockfd->sockfd;
 	int err = connect(sockfd_conn, addr, addrlen);
 	if(err)
 		return err;
+	
+	/* Create another connection with the socket owned by handshake thread of server.
+	 * This is for internal use only and will not be exposed to client app. */
+	sock_descriptor_t *hs = gt_socket(AF_INET, SOCK_STREAM, 0);
+	while(connect(hs->sockfd, addr, addrlen) != 0);
 
-	tcp_packet_t *syn_pkt = (tcp_packet_t *)calloc(1, sizeof(tcp_packet_t));
-	syn_pkt->ubuf = NULL;
-	syn_pkt->ulen = 0;
-	syn_pkt->uflags = 0;
-	syn_pkt->gt_flags |= SYN_FLAG;
-	gt_send_size(sockfd_conn, (void *)syn_pkt);
-//SYN is sent at this point
-
-	tcp_packet_t *syn_ack_pkt = NULL;
-	gt_recv_size(sockfd_conn, &syn_ack_pkt);
-	assert((syn_ack_pkt->gt_flags & SYN_FLAG) && (syn_ack_pkt->gt_flags & ACK_FLAG));
-//SYN, ACK is received at this point
-
-	tcp_packet_t *ack_pkt = (tcp_packet_t *)calloc(1, sizeof(tcp_packet_t));
-	ack_pkt->ubuf = NULL;
-	ack_pkt->ulen = 0;
-	ack_pkt->uflags = 0;
-	ack_pkt->gt_flags |= ACK_FLAG;
-	gt_send_size(sockfd_conn, (void *)ack_pkt);
-
-	return err;
+	/* Fork the handshake thread on connect side. Pass on the cookie, data and socket to the
+	 * handshake thread along with pid of main thread to communicate back (via signaling)  */
+	pthread_t hs_thread;
+	thread_args_t *args = (thread_args_t *) calloc(1, sizeof(thread_args_t));
+	args->app_tid = pthread_self();
+	args->appd = *sockfd;
+	args->hsd = *hs;
+	args->cookie = cookie;
+	args->ulen = ulen;
+	args->udata = (char *) malloc(ulen * sizeof(char));
+	memcpy(args->udata, udata, ulen);
+	pthread_create(&hs_thread, NULL, gt_connect_handshake_thread, args);
+	return 0;
 }
 
 sock_descriptor_t * gt_accept(sock_descriptor_t *sockfd, struct sockaddr *addr, socklen_t *addrlen) 
@@ -90,6 +89,8 @@ sock_descriptor_t * gt_accept(sock_descriptor_t *sockfd, struct sockaddr *addr, 
 }
 
 ssize_t gt_send(sock_descriptor_t *sockfd, const void *buf, size_t len, int flags) {
+	if(sockfd == NULL)
+		return -1;
 	tcp_packet_t *send_pkt = (tcp_packet_t *) calloc(1, sizeof(tcp_packet_t));
 	send_pkt->ubuf = (char *) malloc(len * sizeof(char));
 	memcpy(send_pkt->ubuf, buf, len);
@@ -100,6 +101,8 @@ ssize_t gt_send(sock_descriptor_t *sockfd, const void *buf, size_t len, int flag
 }
 
 ssize_t gt_recv(sock_descriptor_t *sockfd, void *buf, size_t len, int flags) {
+	if(sockfd == NULL)
+		return -1;
 	/* check if we have anything to return from buffer */
 	ssize_t available = sockfd->length - sockfd->offset;
 	if(available <= 0)
@@ -145,4 +148,49 @@ int gt_close(sock_descriptor_t *sockfd)
 		sockfd->offset = 0;
 	}
 	return close(sockfd->sockfd);
+}
+
+
+void * gt_connect_handshake_thread(void * arguments)
+{
+	thread_args_t * args = (thread_args_t *) arguments;
+
+	tcp_packet_t *syn_pkt = (tcp_packet_t *)calloc(1, sizeof(tcp_packet_t));
+	syn_pkt->ulen = args->ulen;
+	syn_pkt->ubuf = (char *) malloc(args->ulen * sizeof(char));
+	memcpy(syn_pkt->ubuf, args->udata, args->ulen);
+	syn_pkt->cookie = args->cookie;
+	syn_pkt->uflags = 0;
+	syn_pkt->gt_flags |= SYN_FLAG;
+	if(syn_pkt->cookie)	syn_pkt->gt_flags |= COOKIE_REQ_FLAG;
+	else			syn_pkt->gt_flags |= FAST_OPEN_FLAG;
+
+	gt_send_size(args->hsd.sockfd, (void *)syn_pkt);
+//SYN is sent at this point
+
+	tcp_packet_t *syn_ack_pkt = NULL;
+	gt_recv_size(args->hsd.sockfd, &syn_ack_pkt);
+	if(! ((syn_ack_pkt->gt_flags & SYN_FLAG) && (syn_ack_pkt->gt_flags & ACK_FLAG)) )
+	{
+		/* Failed handshake - close the connection(s) and die */
+		gt_close(&args->appd);
+		gt_close(&args->hsd);
+		free(args);
+		pthread_exit(NULL);
+	}
+	//SYN, ACK is received at this point
+
+	/* gt_flags in syn_ack_pkt may or may not have FAST_OPEN_FLAG depending on whether
+	 * this is a Fast open or not */
+
+	tcp_packet_t *ack_pkt = (tcp_packet_t *)calloc(1, sizeof(tcp_packet_t));
+	ack_pkt->ubuf = NULL;
+	ack_pkt->ulen = 0;
+	ack_pkt->uflags = 0;
+	ack_pkt->gt_flags |= ACK_FLAG;
+	gt_send_size(args->hsd.sockfd, (void *)ack_pkt);
+
+	/* handshake successful - die silently */
+	free(args);
+	pthread_exit(NULL);
 }
--- tcp-fast-open/tcp.h
+++ tcp-fast-open/tcp.h
@@ -4,10 +4,13 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <stdint.h>
+#include <unistd.h>
 
 #define SYN_FLAG 1
 #define ACK_FLAG 2
 #define FIN_FLAG 4
+#define COOKIE_REQ_FLAG 8
+#define FAST_OPEN_FLAG 16
 
 typedef struct _tcp_packet 
 {
@@ -15,6 +18,7 @@ typedef struct _tcp_packet
 	size_t ulen;
 	int uflags;
 	uint32_t gt_flags;
+	unsigned long cookie;
 }tcp_packet_t;
 
 typedef struct _sock_descriptor
@@ -25,13 +29,25 @@ typedef struct _sock_descriptor
 	ssize_t length;
 }sock_descriptor_t;
 
+typedef struct _thread_args
+{
+	pid_t app_tid;
+	sock_descriptor_t appd;
+	sock_descriptor_t hsd;
+	unsigned long cookie;
+	char *udata;
+	ssize_t ulen;
+}thread_args_t;
+
 sock_descriptor_t * gt_socket(int domain, int type, int protocol);
 int gt_listen(sock_descriptor_t * sockfd, int backlog);
 int gt_bind(sock_descriptor_t * sockfd, const struct sockaddr *addr, socklen_t addrlen);
-int gt_connect(sock_descriptor_t * sockfd, const struct sockaddr *addr, socklen_t addrlen);
+int gt_connect(sock_descriptor_t *sockfd, const struct sockaddr *addr, socklen_t addrlen, unsigned long cookie, char * udata, ssize_t ulen);
 sock_descriptor_t *gt_accept(sock_descriptor_t * sockfd, struct sockaddr *addr, socklen_t *addrlen);
 ssize_t gt_send(sock_descriptor_t * sockfd, const void *buf, size_t len, int flags);
 ssize_t gt_recv(sock_descriptor_t * sockfd, void *buf, size_t len, int flags);
 int gt_close(sock_descriptor_t * sockfd);
+
+void * gt_connect_handshake_thread(void * arguments);
 
 #endif
